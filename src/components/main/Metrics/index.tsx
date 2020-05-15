8980c03bd2717a1214491ac32c9b79e1e54225cd
Fedor Katurov <gotham48@gmail.com>
2020-05-15 09:06:49 +0700

    fixed yandex metrica loading

--- src/components/main/Metrics/index.tsx
+++ src/components/main/Metrics/index.tsx
@@ -22,6 +22,20 @@ const Metrics: FC<IProps> = ({}) => {
           gtag('config', 'UA-81805223-9');
         `}
         </script>
+
+        <script type="text/javascript">
+          {`
+          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+          ym(62732482, "init", {
+            clickmap:true,
+            trackLinks:true,
+            accurateTrackBounce:true,
+            webvisor:true
+          });
+        `}
+        </script>
       </Helmet>
 
       <noscript>
@@ -32,20 +46,6 @@ const Metrics: FC<IProps> = ({}) => {
             alt=""
           />
         </div>
-
-        <script type="text/javascript">
-          {`
-          (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-          m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-          ym(62732482, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-          });
-        `}
-        </script>
       </noscript>
     </>
   )
